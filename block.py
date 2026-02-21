5fd86b2ce8c4438481fb97a8fdb7f93687c25136
hippo1308 <tushi1308@gmail.com>
2026-02-21 17:55:10 -0500

    individual blocks

---
+++ block.py
@@ -0,0 +1,13 @@
+from colours import Colours
+
+
+class Block:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cellSize = 30
+        self.rotationState = 0
+        self.colours = Colours.getCellColours()
+
+    def draw(self, screen):
+
